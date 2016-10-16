5c93dd5fa6e979561f8689566713577274957bd6
susanwolfgram <skwolf@uw.edu>
2016-10-16 00:29:34 -0700

    asdf

--- penny.js
+++ penny.js
@@ -72,7 +72,7 @@ app.controller("myCtrl", function($scope, $firebaseObject, $firebaseArray, $fire
     } else {
     // var commentArr = $firebaseArray(post.comments);
     // commentArr.push(comment); 
-    post.comments += comment; 
+    post.comments += [comment]; 
     $scope.posts.$save(post); 
     // $scope.songs = []; 
     // var baz = new Firebase("https://penny-fyt-123.firebaseio.com/posts/" + post.$id + "/" + post.comment);      
