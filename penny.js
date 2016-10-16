33dadd7e59b1577ca91d1bb546c7c79f8ce02e93
susanwolfgram <skwolf@uw.edu>
2016-10-15 22:59:05 -0700

    asdf

--- penny.js
+++ penny.js
@@ -1,8 +1,12 @@
 var app = angular.module("myApp", ["firebase"]);
 
 app.controller("myCtrl", function($scope, $firebaseObject) {
-  var ref = firebase.database().ref();
+  var ref = firebase.database().ref().child("data");
+  var syncObject = $firebaseObject(ref);
+  // synchronize the object with a three-way data binding
+  // click on `index.html` above to see it used in the DOM!
+  syncObject.$bindTo($scope, "data");
   // download the data into a local object
-  $scope.data = $firebaseObject(ref);
+  //$scope.data = $firebaseObject(ref);
   // putting a console.log here won't work, see below
 });
