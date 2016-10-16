027b9644b0fdf45bc0973f144949cff03040a376
susanwolfgram <skwolf@uw.edu>
2016-10-15 23:04:21 -0700

    alskdf

--- penny.js
+++ penny.js
@@ -1,12 +1,24 @@
 var app = angular.module("myApp", ["firebase"]);
 
 app.controller("myCtrl", function($scope, $firebaseObject) {
-  var ref = firebase.database().ref().child("data");
-  var syncObject = $firebaseObject(ref);
+  //var ref = firebase.database().ref().child("data");
+  //var syncObject = $firebaseObject(ref);
   // synchronize the object with a three-way data binding
   // click on `index.html` above to see it used in the DOM!
-  syncObject.$bindTo($scope, "data");
+  //syncObject.$bindTo($scope, "data");
   // download the data into a local object
   //$scope.data = $firebaseObject(ref);
   // putting a console.log here won't work, see below
+  var ref = firebase.database().ref().child("messages");
+  // create a synchronized array
+  $scope.messages = $firebaseArray(ref);
+  // add new items to the array
+  // the message is automatically added to our Firebase database!
+  $scope.addMessage = function() {
+    $scope.messages.$add({
+      text: $scope.newMessageText
+    });
+  };
+
+
 });
