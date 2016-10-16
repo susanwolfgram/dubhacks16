165ee184b9f7edb43bcfae09fdedb3d44e2fd5a4
susanwolfgram <skwolf@uw.edu>
2016-10-16 10:11:43 -0700

    php added

--- penny.php
+++ penny.php
@@ -49,11 +49,11 @@
 			<button class="md-button" ng-click="signOut()" ng-show="handle">Sign Out</button>
 			<div class="head-circle" style="float: left;"></div>	
 			 <div class="dropdown">
-  				<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example</button>
+  				<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">My Account</button>
  				 <ul class="dropdown-menu">
-				    <li><a href="#">HTML</a></li>
-				    <li><a href="#">CSS</a></li>
-				    <li><a href="#">JavaScript</a></li>
+				    <li><a href="#">Credits</a></li>
+				    <li><a href="#">Log Out</a></li>
+				    <!-- <li><a href="#">JavaScript</a></li> -->
   				</ul>
 			</div>
 		</nav>
