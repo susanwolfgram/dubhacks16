2672ec78d24ce7f6a511f80c6aabc6d7e3b2e1ed
susanwolfgram <skwolf@uw.edu>
2016-10-16 12:08:24 -0700

    merge?

--- penny.php
+++ penny.php
@@ -111,7 +111,7 @@
 		        <p>{{post.text}} {{post.cents}} cents</p>
 		        <img ng-show="post.image != ''" src="{{post.image}}" alt="image" width="500px"  />
 		        <br />
-		        <h3 ng-click="displayComments(post)">Open Comments</h3>
+		        <h3 ng-show="post.comments != 0" ng-click="displayComments(post)">Open Comments</h3>
 		        <div id="comments" ng-show="yesComments" >
 		        	<hr />
 		        	<p ng-repeat="comment in commentArr">{{comment.comment}} <span class="commenter">-{{comment.user}}</span></p><hr /> 
