10662670a60b8c080221f06b38ef6f92136d222f
susanwolfgram <skwolf@uw.edu>
2016-10-16 14:51:36 -0700

    merge?

--- penny.js
+++ penny.js
@@ -205,8 +205,8 @@ app.controller("myCtrl", function($scope, $firebaseObject, $firebaseArray, $fire
 
 	//$scope.yesComments = false; 
 
-	$scope.displayComments = function(post) {
-		$scope.yesComments= !$scope.yesComments; 
+	$scope.displayComments = function(post, time) {
+		$scope.yesComments+time= !$scope.yesComments+time; 
 		$scope.commentArr = post.comments;
 		$scope.$digest();
 	}
