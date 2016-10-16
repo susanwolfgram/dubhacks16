afcfa06569dbfa49ab56b3f480c469aa6f246de7
susanwolfgram <skwolf@uw.edu>
2016-10-16 10:31:21 -0700

    php added

--- penny.js
+++ penny.js
@@ -22,7 +22,7 @@ app.controller("myCtrl", function($scope, $firebaseObject, $firebaseArray, $fire
 	// 	});
 	// }
 	
-	$scope.load = (function () {
+	$scope.load = function () {
         if (getCookie("user") != "") {
         	user = getCookie("user"); 
         	$scope.userEmail = $scope.email; 
@@ -32,13 +32,10 @@ app.controller("myCtrl", function($scope, $firebaseObject, $firebaseArray, $fire
 			$scope.handle = true; 
 			ref = firebase.database().ref().child("posts");
 	     	$scope.$digest();
-	     	//$scope.credits = userObj.credit; 
-	     	//console.log(userObj.credit);
+	     	$scope.credits = userObj.credit; //not registering... need async function
+	     	console.log(userObj.credit);
         }
-    }).then(function() {
-		$scope.credits = userObj.credit; 
-     	console.log(userObj.credit);
-    });
+    }
 
  //    function setCookie(cname, cvalue, exdays) {
 	//     //var d = new Date();
